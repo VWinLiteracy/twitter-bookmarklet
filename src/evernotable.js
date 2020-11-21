f2f07b97903de631e1ddf46ea0b2b1ee8d1b82f7
Akunohadou <vwinliteracy@gmail.com>
2020-11-21 15:16:29 +0900

    仕様変更対応

--- src/evernotable.js
+++ src/evernotable.js
@@ -41,7 +41,7 @@
         if (parts.length == 0) {
             return;
         }
-        var parts2 = document.getElementsByClassName('css-1dbjc4n r-9x6qib r-1867qdf r-1phboty r-rs99b7 r-156q2ks r-1ny4l3l r-1udh08x r-o7ynqc r-6416eg');
+        var parts2 = document.getElementsByClassName('css-1dbjc4n r-1bs4hfb r-1867qdf r-1phboty r-rs99b7 r-156q2ks r-1ny4l3l r-1udh08x r-o7ynqc r-6416eg');
         parts2[0].insertBefore(parts[0], parts2[0].childNodes[0]);
     }
 
@@ -57,12 +57,13 @@
     removeFrontLayer();
     removeHeader();
     removeSidebar();
-    removeElements('css-1dbjc4n r-aqfbo4 r-14lw9ot r-my5ep6 r-rull8r r-qklmqi r-gtdqiz r-ipm5af r-1g40b8q'); // tweet header
+    removeElements('css-1dbjc4n r-aqfbo4 r-14lw9ot r-j7yic r-rull8r r-qklmqi r-gtdqiz r-ipm5af r-1g40b8q'); // tweet header
     removeElements('css-1dbjc4n r-9x6qib r-1867qdf r-rs99b7 r-1loqt21 r-dap0kf r-1ny4l3l r-1udh08x r-o7ynqc r-6416eg'); // include tweet
     removeElements('css-1dbjc4n r-1loqt21 r-18u37iz r-1ny4l3l r-o7ynqc r-6416eg'); // reply
     removeElements('css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-1mi0q7o');  // private reply
     removeElements('css-18t94o4 css-1dbjc4n r-1ny4l3l r-1j3t67a r-o7ynqc r-6416eg'); // omitted reply
-    removeElements('css-1dbjc4n r-1awozwy r-zv2cs0 r-1867qdf r-18u37iz r-ku1wi2 r-156q2ks r-1yt7n81 r-9qu9m4'); // reply restricted
+    removeElements('css-1dbjc4n r-1awozwy r-zv2cs0 r-1867qdf r-18u37iz r-ku1wi2 r-156q2ks r-1yt7n81 r-9qu9m4'); // reply restricted tweet
+    removeElements('css-18t94o4 css-1dbjc4n r-16y2uox r-1n0xq6e r-1ny4l3l r-qhyqy2 r-atwnbb r-o7ynqc r-6416eg'); // show reply
     removeElements('css-1dbjc4n r-18u37iz r-d0pm55'); // user link
     removeElements('css-1dbjc4n r-xoduu5 r-1mlwlqe r-1d2f490 r-1udh08x r-u8s1d r-h9hxbl r-417010'); // emoji
     restoreOriginalImage();
